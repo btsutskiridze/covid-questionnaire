bcbf4de7006c1b8d499e79c46df0abf37f856200
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-10-10 13:21:43 +0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid place-items-center h-screen">
+    <h1>კოვიდი</h1>
+  </div>
+</template>
+
+<style>
+@font-face {
+  font-family: "BGP Arial";
+  src: local("BGP Arial"), url(./fonts/bpg_arial_2009.ttf) format("truetype");
+}
+
+* {
+  font-family: "BGP Arial";
+}
+</style>
