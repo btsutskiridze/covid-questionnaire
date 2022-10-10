a17f2e5ee4fbbb5b8412ee66b4895d3e8ecafa99
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-10-10 22:03:37 +0400

    feat: add components for input fields

---
+++ src/components/form/RadioInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-row items-center mb-5">
+    <input
+      type="radio"
+      :id="value"
+      :name="name"
+      :value="value"
+      class="w-6 h-6 mr-5 accent-[#232323]"
+    />
+    <label :for="value" class="text-xl font-bold">{{ labelValue }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "value", "labelValue"],
+  data() {
+    return {};
+  },
+};
+</script>
