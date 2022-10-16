30932b7a95ce74190128e263064301d39ea6fa52
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-10-16 19:26:42 +0400

    feat: display questions according to users choice

--- src/views/CovidQuestionnaireView.vue
+++ src/views/CovidQuestionnaireView.vue
@@ -4,19 +4,44 @@
       <base-question-layout>
         <p>გაქვს გადატანილი Covid-19?*</p>
         <template #options>
-          <base-radio value="yes" name="had_covid" label-value="კი" />
-          <base-radio value="no" name="had_covid" label-value="არა" />
-          <base-radio value="now" name="had_covid" label-value="ახლა მაქვს" />
+          <base-radio
+            value="yes"
+            name="had_covid"
+            label-value="კი"
+            @click="firstQuestion"
+          />
+          <base-radio
+            value="no"
+            name="had_covid"
+            label-value="არა"
+            @click="firstQuestion"
+          />
+          <base-radio
+            value="now"
+            name="had_covid"
+            label-value="ახლა მაქვს"
+            @click="firstQuestion"
+          />
         </template>
       </base-question-layout>
-      <base-question-layout v-if="false">
+      <base-question-layout v-if="hadCovid">
         <p>ანტისხეულების ტესტი გაქვს გაკეთებული?*</p>
         <template #options>
-          <base-radio value="yes" name="had_antibody_test" label-value="კი" />
-          <base-radio value="no" name="had_antibody_test" label-value="არა" />
+          <base-radio
+            value="yes"
+            name="had_antibody_test"
+            label-value="კი"
+            @click="secondQuestion"
+          />
+          <base-radio
+            value="no"
+            name="had_antibody_test"
+            label-value="არა"
+            @click="secondQuestion"
+          />
         </template>
       </base-question-layout>
-      <base-question-layout v-if="false">
+      <base-question-layout v-if="hadAntibodyTest">
         <p class="leading-7 whitespace-nowrap">
           თუ გახსოვს, გთხოვ მიუთითე ტესტის მიახლოებითი <br />
           რიცხვი და ანტისხეულების რაოდენობა*
@@ -28,7 +53,7 @@
           </div>
         </template>
       </base-question-layout>
-      <base-question-layout v-if="false">
+      <base-question-layout v-if="hadAntibodyTest === false">
         <p>
           მიუთითე მიახლოებითი პერიოდი (დღე/თვე/წელი) <br />
           როდის გქონდა Covid-19*
@@ -68,6 +93,45 @@ export default {
   components: {
     BaseRadio,
     BaseInput,
+  },
+  data() {
+    return {
+      hadCovid: false,
+      hadAntibodyTest: null,
+    };
+  },
+  mounted() {
+    if (localStorage.getItem("hadCovid")) {
+      this.hadCovid = true;
+    }
+    if (localStorage.getItem("hadAntibodyTest") === "yes") {
+      this.hadAntibodyTest = true;
+    } else if (localStorage.getItem("hadAntibodyTest") === "no") {
+      this.hadAntibodyTest = false;
+    }
+  },
+  methods: {
+    firstQuestion(e) {
+      if (e.target.checked && e.target.value === "yes") {
+        localStorage.setItem("hadCovid", e.target.value);
+        this.hadCovid = true;
+      } else {
+        this.hadCovid = false;
+        localStorage.removeItem("hadCovid");
+
+        this.hadAntibodyTest = null;
+        localStorage.removeItem("hadAntibodyTest");
+      }
+    },
+    secondQuestion(e) {
+      if (e.target.checked && e.target.value === "yes") {
+        localStorage.setItem("hadAntibodyTest", e.target.value);
+        this.hadAntibodyTest = true;
+      } else {
+        localStorage.setItem("hadAntibodyTest", e.target.value);
+        this.hadAntibodyTest = false;
+      }
+    },
   },
 };
 </script>
