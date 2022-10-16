67438ab715b546cf6cd725641c1ae6092d0bdaa0
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-10-16 19:25:25 +0400

    feat: save checkbox after page reload

--- src/components/UI/form/BaseRadio.vue
+++ src/components/UI/form/BaseRadio.vue
@@ -6,6 +6,8 @@
       :name="name"
       :value="value"
       class="w-6 h-6 accent-[#232323]"
+      ref="radioInput"
+      @click="saveValue"
     />
     <label
       :for="value"
@@ -20,6 +22,19 @@ export default {
   props: ["name", "value", "labelValue"],
   data() {
     return {};
+  },
+  mounted() {
+    if (
+      localStorage.getItem(this.$refs.radioInput.name) ==
+      this.$refs.radioInput.value
+    ) {
+      this.$refs.radioInput.checked = true;
+    }
+  },
+  methods: {
+    saveValue(e) {
+      localStorage.setItem(e.target.name, e.target.value);
+    },
   },
 };
 </script>
