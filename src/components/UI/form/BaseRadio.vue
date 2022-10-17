292dfef175e704eb47814802dcaa1d07ec2b57ab
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-10-17 12:50:55 +0400

    feat: message *save data on page refresh *display questions according to chosen options

--- src/components/UI/form/BaseRadio.vue
+++ src/components/UI/form/BaseRadio.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="flex flex-row gap-5 items-center justify-start mb-5">
-    <input
-      type="radio"
-      :id="value"
-      :name="name"
-      :value="value"
-      class="w-6 h-6 accent-[#232323]"
-      ref="radioInput"
-      @click="saveValue"
-    />
-    <label
-      :for="value"
-      class="text-xl font-normal whitespace-pre-wrap w-full"
-      >{{ labelValue }}</label
-    >
+    <Field v-slot="{ field }" name="terms">
+      <input
+        v-bind="field"
+        type="radio"
+        :id="value"
+        :name="name"
+        :value="value"
+        class="w-6 h-6 accent-[#232323]"
+        ref="radioInput"
+        @click="saveValue"
+      />
+      <label
+        :for="value"
+        class="text-xl font-normal whitespace-pre-wrap w-full"
+        >{{ labelValue }}</label
+      >
+    </Field>
   </div>
 </template>
 
 <script>
+import { Field } from "vee-validate";
 export default {
+  components: {
+    Field,
+  },
   props: ["name", "value", "labelValue"],
   data() {
     return {};
--- src/views/CovidQuestionnaireView.vue
+++ src/views/CovidQuestionnaireView.vue
@@ -1,112 +1,130 @@
 <template>
-  <base-questions-container page="2">
-    <div>
-      <base-question-layout>
-        <p>გაქვს გადატანილი Covid-19?*</p>
-        <template #options>
-          <base-radio
-            value="yes"
-            name="had_covid"
-            label-value="კი"
-            @click="firstQuestion"
-          />
-          <base-radio
-            value="no"
-            name="had_covid"
-            label-value="არა"
-            @click="firstQuestion"
-          />
-          <base-radio
-            value="now"
-            name="had_covid"
-            label-value="ახლა მაქვს"
-            @click="firstQuestion"
-          />
-        </template>
-      </base-question-layout>
-      <base-question-layout v-if="hadCovid">
-        <p>ანტისხეულების ტესტი გაქვს გაკეთებული?*</p>
-        <template #options>
-          <base-radio
-            value="yes"
-            name="had_antibody_test"
-            label-value="კი"
-            @click="secondQuestion"
-          />
-          <base-radio
-            value="no"
-            name="had_antibody_test"
-            label-value="არა"
-            @click="secondQuestion"
-          />
-        </template>
-      </base-question-layout>
-      <base-question-layout v-if="hadAntibodyTest">
-        <p class="leading-7 whitespace-nowrap">
-          თუ გახსოვს, გთხოვ მიუთითე ტესტის მიახლოებითი <br />
-          რიცხვი და ანტისხეულების რაოდენობა*
-        </p>
-        <template #options>
-          <div class="pl-2">
-            <base-input name="test_date" placeholder="რიცხვი" />
-            <base-input name="number" placeholder="ანტისხეულების რაოდენობა" />
-          </div>
-        </template>
-      </base-question-layout>
-      <base-question-layout v-if="hadAntibodyTest === false">
-        <p>
-          მიუთითე მიახლოებითი პერიოდი (დღე/თვე/წელი) <br />
-          როდის გქონდა Covid-19*
-        </p>
-        <template #options>
-          <div class="pl-2">
-            <base-input name="test_date" placeholder="დდ/თთ/წწ" />
-          </div>
-        </template>
-      </base-question-layout>
-    </div>
-    <template #image>
-      <img
-        class="w-[98%] relative top-[-1.1rem] left-4"
-        src="@/assets/images/questionnaire-view.png"
-        alt="questionnaire-view"
-      />
-    </template>
-    <template #arrows>
-      <div class="flex justify-center fixed left-[45%] bottom-[10%] gap-28">
-        <router-link :to="{ name: 'identification' }">
-          <previous-arrow-icon />
-        </router-link>
-        <router-link :to="{ name: 'vaccination' }">
-          <next-arrow-icon />
-        </router-link>
+  <VeeForm>
+    <base-questions-container page="2">
+      <div>
+        <base-question-layout>
+          <p>გაქვს გადატანილი Covid-19?*</p>
+          <template #options>
+            <base-radio
+              value="yes"
+              name="had_covid"
+              label-value="კი"
+              @click="firstQuestion"
+            />
+            <base-radio
+              value="no"
+              name="had_covid"
+              label-value="არა"
+              @click="firstQuestion"
+            />
+            <base-radio
+              value="now"
+              name="had_covid"
+              label-value="ახლა მაქვს"
+              @click="firstQuestion"
+            />
+          </template>
+        </base-question-layout>
+        <base-question-layout v-if="hadCovid">
+          <p>ანტისხეულების ტესტი გაქვს გაკეთებული?*</p>
+          <template #options>
+            <base-radio
+              :value="true"
+              name="had_antibody_test"
+              label-value="კი"
+              @click="secondQuestion"
+            />
+            <base-radio
+              :value="false"
+              name="had_antibody_test"
+              label-value="არა"
+              @click="secondQuestion"
+            />
+          </template>
+        </base-question-layout>
+        <base-question-layout v-if="hadAntibodyTest">
+          <p class="leading-7 whitespace-nowrap">
+            თუ გახსოვს, გთხოვ მიუთითე ტესტის მიახლოებითი <br />
+            რიცხვი და ანტისხეულების რაოდენობა*
+          </p>
+          <template #options>
+            <div class="pl-2">
+              <base-input name="test_date" placeholder="რიცხვი" />
+              <base-input name="number" placeholder="ანტისხეულების რაოდენობა" />
+            </div>
+          </template>
+        </base-question-layout>
+        <base-question-layout v-if="hadAntibodyTest === false">
+          <p>
+            მიუთითე მიახლოებითი პერიოდი (დღე/თვე/წელი) <br />
+            როდის გქონდა Covid-19*
+          </p>
+          <template #options>
+            <div class="pl-2">
+              <base-input name="covid_date" placeholder="დდ/თთ/წწ" />
+            </div>
+          </template>
+        </base-question-layout>
       </div>
-    </template>
-  </base-questions-container>
+      <template #image>
+        <img
+          class="w-[98%] relative top-[-1.1rem] left-4"
+          src="@/assets/images/questionnaire-view.png"
+          alt="questionnaire-view"
+        />
+      </template>
+      <template #arrows>
+        <div class="flex justify-center fixed left-[45%] bottom-[10%] gap-28">
+          <router-link :to="{ name: 'identification' }">
+            <previous-arrow-icon />
+          </router-link>
+          <button>
+            <next-arrow-icon />
+          </button>
+        </div>
+      </template>
+    </base-questions-container>
+  </VeeForm>
 </template>
 
 <script>
 import BaseRadio from "@/components/UI/form/BaseRadio.vue";
 import BaseInput from "@/components/UI/form/BaseInput.vue";
+import { Form as VeeForm } from "vee-validate";
 
 export default {
   components: {
     BaseRadio,
     BaseInput,
+    VeeForm,
   },
   data() {
     return {
       hadCovid: false,
       hadAntibodyTest: null,
+      schema: {
+        had_covid: (value) => {
+          if (value) {
+            return true;
+          }
+          return false;
+        },
+        had_antibody_test: (value) => {
+          if (value) {
+            return true;
+          }
+          return false;
+        },
+      },
     };
   },
   mounted() {
     if (localStorage.getItem("hadCovid")) {
       this.hadCovid = true;
     }
-    if (localStorage.getItem("hadAntibodyTest") === "yes") {
+    if (localStorage.getItem("hadAntibodyTest") === "true") {
       this.hadAntibodyTest = true;
-    } else if (localStorage.getItem("hadAntibodyTest") === "no") {
+    } else if (localStorage.getItem("hadAntibodyTest") === "false") {
       this.hadAntibodyTest = false;
     }
   },
@@ -121,10 +139,11 @@ export default {
 
         this.hadAntibodyTest = null;
         localStorage.removeItem("hadAntibodyTest");
+        localStorage.removeItem("had_antibody_test");
       }
     },
     secondQuestion(e) {
-      if (e.target.checked && e.target.value === "yes") {
+      if (e.target.checked && e.target.value === "true") {
         localStorage.setItem("hadAntibodyTest", e.target.value);
         this.hadAntibodyTest = true;
       } else {
