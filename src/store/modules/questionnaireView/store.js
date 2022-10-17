bf506a75adfb655031359a2979ef9da967eebea4
btsutskiridze <bakatsutskiridze@redberry.ge>
2022-10-17 13:50:46 +0400

    feat: function to validate date

--- src/store/modules/questionnaireView/store.js
+++ src/store/modules/questionnaireView/store.js
@@ -1,8 +1,31 @@
 export default {
   state() {
-    return {};
+    return {
+      had_covid: "",
+      had_antibody_test: "",
+      test_date: "",
+      number: "",
+      covid_date: "",
+    };
   },
 
   mutations: {},
-  actions: {},
+  actions: {
+    dateValidation(_, value) {
+      const inputDate = new Date(value); //taking input as date
+      const minDate = new Date("2020-01-01").getTime(); //converting min data to millsec
+      const maxDate = new Date().getTime(); //converting max data to millsec
+
+      //comparing inputDate to limits
+      if (
+        inputDate.toDateString() === "Invalid Date" ||
+        inputDate.getTime() < minDate ||
+        inputDate.getTime() > maxDate
+      ) {
+        return "გთხოვთ მიუთითოთ სწორი თარიღი";
+      } else {
+        return true;
+      }
+    },
+  },
 };
